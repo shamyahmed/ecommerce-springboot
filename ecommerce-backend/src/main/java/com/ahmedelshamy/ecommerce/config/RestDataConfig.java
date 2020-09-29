df69d9a0ce8831c4eca4e50283c96a22ff5bc866
shamy88 <eng.shamy88@gmail.com>
2020-09-29 19:24:55 +0200

    v1.0.8 - backend - expose REST entity ids

--- ecommerce-backend/src/main/java/com/ahmedelshamy/ecommerce/config/RestDataConfig.java
+++ ecommerce-backend/src/main/java/com/ahmedelshamy/ecommerce/config/RestDataConfig.java
@@ -2,17 +2,31 @@ package com.ahmedelshamy.ecommerce.config;
 
 import com.ahmedelshamy.ecommerce.entity.Product;
 import com.ahmedelshamy.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class RestDataConfig implements RepositoryRestConfigurer {
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        disableAllHttpExceptGet(config);
+        exposeIds(config);
+    }
 
+    private void disableAllHttpExceptGet(RepositoryRestConfiguration config) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
@@ -26,6 +40,13 @@ public class RestDataConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
 
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+        entities.forEach(entity -> entityClasses.add(entity.getJavaType()));
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
