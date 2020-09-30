da622934549cdde5dad331424368c8ff374a89df
shamy88 <eng.shamy88@gmail.com>
2020-09-30 15:58:30 +0200

    v1.0.16 - frontend - shopping cart details page & increment, decrement, remove features

--- ecommerce-frontend/src/app/services/cart.service.ts
+++ ecommerce-frontend/src/app/services/cart.service.ts
@@ -69,4 +69,26 @@ export class CartService {
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('----');
   }
+
+  decrementQuantity(cartItem: CartItem) {
+    cartItem.quantity--;
+
+    if (cartItem.quantity === 0) {
+      this.remove(cartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(cartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === cartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
 }
